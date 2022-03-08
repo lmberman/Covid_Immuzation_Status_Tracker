8055be2624257fa90db69ca5fc14eb208d991422
lmberman <bermanlindsei@gmail.com>
2022-03-07 20:05:33 -0500

    Update Spring Security to exclude H2 Db since H2 has its own security protocols

--- src/main/java/edu/bowiestate/covidTracker/SecurityConfig.java
+++ src/main/java/edu/bowiestate/covidTracker/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,14 +21,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     DataSource dataSource;
 
     @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
                     .antMatchers("/").authenticated()
                     .antMatchers("/home").hasAnyRole("CEO","CSRA","EMPLOYEE","CUSTOMER")
-                    .antMatchers("/console/**").permitAll()
                     .antMatchers("/logout").permitAll()
-                    .and()
+                .and()
                 .formLogin()
                     .loginPage("/login")
                     .permitAll()
